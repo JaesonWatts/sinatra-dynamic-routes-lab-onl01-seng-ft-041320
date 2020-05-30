770826ea247e784f3b911ab24e882eb24c311ba0
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-30 06:00:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,30 +1,30 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Create a dynamic route at get '/reversename/:name' that accepts a name and renders the name
+  # accepts a name and renders the name
   backwards.
   
   get "/reversename/:name" do
     
   end 
 
-# Create a dynamic route at get '/square/:number' that accepts a number and returns the square of that number. Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
+#accepts a number and returns the square of that number. Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
   
   get "/square/:number" do
   end
   
 
-# Create a dynamic route at get '/say/:number/:phrase' that accepts a number and a phrase and returns that phrase in a single string the number of times given.
+#  accepts a number and a phrase and returns that phrase in a single string the number of times given.
 
   get "/say/:number/:phrase" do 
   end
 
-# Create a dynamic route at get '/say/:word1/:word2/:word3/:word4/:word5' that accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
+#  accepts five words and returns a string containing all five words (i.e. word1 word2 word3 word4 word5).
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
   end
   
-# Create a dynamic route at get '/:operation/:number1/:number2' that accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided, returning a String. For example, going to /add/1/2 should render 3 as a String.
+#  accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided, returning a String. For example, going to /add/1/2 should render 3 as a String.
 
   get "/:operation/:number1/:number2" do
   end
