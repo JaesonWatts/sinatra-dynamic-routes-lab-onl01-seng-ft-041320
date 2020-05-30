46b09a35c809dd616cb90dc3972d942631b9975c
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-30 07:23:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,11 +18,11 @@ class App < Sinatra::Base
 #  accepts a number and a phrase and returns that phrase in a single string the number of times given.
 
   get "/say/:number/:phrase" do 
-    # thismany = params[:number].to_i
-    # binding.pry
-    # thismany.each do 
-    #   params[:phrase]
-    # end
+    thismany = params[:number].to_i
+    binding.pry
+    thismany.each do 
+      params[:phrase]
+    end
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
@@ -30,8 +30,6 @@ class App < Sinatra::Base
     string
   end
   
-#  accepts an operation (add, subtract, multiply or divide) and performs the operation on the two numbers provided, returning a String. For example, going to /add/1/2 should render 3 as a String.
-
   get "/:operation/:number1/:number2" do
     if params[:operation] == "add"
         result = params[:number1].to_i + params[:number2].to_i
