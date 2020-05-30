815bb13c546b47132f356ae1a12589c1506ce059
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-30 06:03:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ class App < Sinatra::Base
   get "/reversename/:name" do
     a = params[:name]
     a.reverse!
+    a
   end 
 
 #accepts a number and returns the square of that number. Note: Remember that values in params always come in as strings, and your return value for the route should also be a string (use .to_i and .to_s).
