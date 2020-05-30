fcf25ddb6b0c3c5b611e4a01a8f322ac2223c6ef
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-30 07:30:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,14 +17,13 @@ class App < Sinatra::Base
 
 #  accepts a number and a phrase and returns that phrase in a single string the number of times given.
 
-  get "/say/:number/:phrase" do 
-    thismany = params[:number].to_i
-    phrase = params[:phrase]
-    thismany.times do 
-      phrase += params[:phrase]
-      phrase
-    end
-  end
+@number = params[:number].to_i
+		phrase = params[:phrase]
+		string = ""
+		number.times do
+			string += phrase
+		end
+		string
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
     string = params[:word1] + " " + params[:word2] + " " + params[:word3] + " " + params[:word4] + " " + params[:word5] + "."
