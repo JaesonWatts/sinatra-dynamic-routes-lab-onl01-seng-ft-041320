34c0e556f8166f94fec400e8ae7bf89e10819b7c
Jaeson Watts <jaeson.watts@gmail.com>
2020-05-30 07:26:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
     thismany = params[:number].to_i
     phrase = params[:phrase]
     thismany.times do 
-      puts phrase
+      phrase
     end
     #binding.pry
   end
